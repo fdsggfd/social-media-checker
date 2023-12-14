2d2f73503583409a34a161848e174f704deaebbc
Sipi <112963011+fdsggfd@users.noreply.github.com>
2023-12-14 16:07:59 +0100

    Update Social-Media-Checker.py

--- Social-Media-Checker.py
+++ Social-Media-Checker.py
@@ -7,29 +7,27 @@ class ChecK():
         self.twitter()
 
     def PrintT(self):
-        print(f"{self.email} = Linked"+"\n")
+        print(f"{self.email} = Linked" + "\n")
 
     def PrintF(self):
-        print(f"{self.email} = Unlinked"+"\n")
+        print(f"{self.email} = Unlinked" + "\n")
 
     def twitter(self):
         print("==================")
         print("[+] Twitter [+]")
         print("")
         r = requests.Session()
-        url = "https://api.twitter.com/i/users/email_available.json?email="+self.email
+        url = "https://api.twitter.com/i/users/email_available.json?email=" + self.email
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
         Host = "api.twitter.com"
         Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-        r.headers = {'User-Agent': user_agent}
-        r.headers = {'Host': Host}
-        r.headers = {'Accept': Accept}
+        r.headers = {'User-Agent': user_agent, 'Host': Host, 'Accept': Accept}
         req = r.get(url).json()
         text = str(req)
         print(text)
         print('')
         if text.find("'valid': False") == True:
-            self.PrintT() 
+            self.PrintT()
         else:
             self.PrintF()
         self.instagram()
@@ -43,67 +41,53 @@ class ChecK():
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
         r.headers = {'user-agent': user_agent}
         r.headers.update({'X-CSRFToken': "missing"})
-        data = {"email_or_username":self.email}
-        req = r.post(url,data=data)
+        data = {"email_or_username": self.email}
+        req = r.post(url, data=data)
         print(req.text)
         print('')
-        if req.text.find("We sent an self.email to")>=0:
+        if req.text.find("We sent an self.email to") >= 0:
             self.PrintT()
-        elif req.text.find("password")>=0:
+        elif req.text.find("password") >= 0:
             self.PrintT()
-        elif req.text.find("sent")>=0:
+        elif req.text.find("sent") >= 0:
             self.PrintT()
         else:
             self.PrintF()
-        self.snacphat()
+        self.snapchat()
 
-    def snacphat(self): 
+    def snapchat(self):
         print("==================")
         print("[+] Snapchat [+]")
         print("")
+        url = "https://bitmoji.api.snapchat.com/api/user/find"
         r = requests.Session()
-        url = "https://accounts.snapchat.com/accounts/merlin/login"
         r.headers = {
-        'Host': 'accounts.snapchat.com',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'X-XSRF-TOKEN': 'missing',
-        'Content-Type': 'application/json',
-        'Origin': 'https://accounts.snapchat.com',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-        'Connection':'keep-alive',
-        'Referer': 'https://accounts.snapchat.com/accounts/merlin/login'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         }
-        cookies = {
-        'xsrf_token':'missing'
-        }
-        data = {
-        'email':self.email,
-        'app':'BITMOJI_APP'
-        }
-        req = r.post(url, cookies=cookies, json=data)
-        print(req.text) # If the response is blank, it means Unlinked .
+        req = r.post(url, headers=r.headers, json={"email": self.email})
+        status = req.status_code
+        print(status)
         print('')
-        if req.text.find("hasSnapchat") >= 0 :
-            self.PrintT()
+        if status == 200:
+            print(f"{self.email} = is linked to an account" + "\n")
+            file = open("snapchat-linked.txt", "a")
+            file.write(self.email + "\n")
+            file.close()
         else:
-            self.PrintF()
-        
+            print(f"{self.email} = is not linked to an account" + "\n")
 
 
 if __name__ == "__main__":
     print("""
              [-] SocialMediaChecker [-]
-          [ Twitter - Instagram - Snapchat ]
+              [ Twitter - Instagram - Snapchat ]
         ======================================= 
-        [+] Programming By : Rahul Kant .     |
-        [+] Instagram: @imrahulkant .         |
-        [+] Github : imrahulkant .            |
+        [+] Run by : DigiHub            |
         =======================================
         """)
     ChecK()
 
-
-print('')    
+print('')
 print('Press enter to exit .')
 input('')
